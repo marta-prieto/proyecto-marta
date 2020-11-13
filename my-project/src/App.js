76860cc53c6ddca825adcc5d5ee85d88f0957d8f
Marta Prieto Luque <marta.prieto@garajedeideas.com>
2020-11-13 22:01:15 +0100

    add first structure

---
+++ my-project/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import DogCards from './components/header';
+
+class App extends React.Component {
+  render() {
+    return ( < React.Fragment >
+
+      <
+      DogCards / >
+
+      <
+      /React.Fragment >
+    )
+  }
+}
+
+export default App;
---
+++ my-project/src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class DogCards extends React.Component {
+
+  render() {
+    return ( <
+      div className = "curso" >
+      <
+      h1 > ESTE CÓDIGO ES DE PRUEBA < /h1> < /
+      div >
+
+    );
+  }
+}
+export default DogCards;
