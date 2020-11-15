0476090ffa98a442f1a2ed7af73c8714d11e5bad
totegsito <pacoquesmun@gmail.com>
2020-11-15 13:49:22 +0100

    server to allow requests to lamadrilenia and galgos112

---
+++ server/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    madrilenia: {
+        url: 'https://adopcioneslamadrilena.org/listado',
+        pageParam: 'p',
+    },
+    galgos112: {
+        url: 'https://www.galgos112.es/lang-es/pag/adopta/',
+        pageParam: 'pag'
+    }
+}
---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const axios = require('axios');
+const cors = require('cors');
+const config = require('./config')
+const app = express();
+const port = 5000;
+
+app.use(cors());
+
+app.get("/:web/:page?", async(req, res) => {
+    try {
+        const { web, page = 1 } = req.params
+        const { [web]: myWeb} = config
+        const response = await axios.get(`${myWeb.url}?${myWeb.pageParam}=${page}`);
+        console.log(response)
+        return res.status(200).send(response.data);
+    } catch ({ message }) {
+        console.log(message)
+        res.status(500).send({ message })
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
